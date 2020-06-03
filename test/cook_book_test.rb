da34f9e969ab1d777ed63808b81974508470ce71
“Neeru <neeram85@gmail.com>
2020-06-03 11:28:59 -0600

    Complete CookBook class

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -13,5 +13,20 @@ class CookBookTest < Minitest::Test
     @cookbook = CookBook.new
   end
 
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @cookbook.recipes
+  end
+
+  def test_it_can_add_recipes
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
 
 end
